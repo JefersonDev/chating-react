080f1986d70f38d7ed6e4de9a44a7965d1449792
Jeferson <jefersonqueirozdev@gmail.com>
2021-01-01 22:29:35 -0300

    interactions and sam fakes contacts

--- src/application/App.js
+++ src/application/App.js
@@ -10,15 +10,97 @@ import '../styles/right/header.css'
 import '../styles/right/plaintext.css'
 import '../styles/right/wrappers.css'
 import React from "react";
+
 class App extends react.Component {
 
     constructor(props) {
         super(props);
-        this.state = {profile: {id: '', avatar: '', username: '', status: ''}}
+        this.state = {
+            profile: {id: '123456798', avatar: 'img/afro.png', username: 'username', status: 'online'},
+            conversation: {to: undefined, room: undefined},
+            handler: 'contacts',
+
+            conversations: [
+                {room: "321", by: {id:"987",avatar: "img/punk.png", username: "punk", status: "online"}},
+                {room: "332", by: {id:"741",avatar: "img/woman.png", username: "woman", status: "busy"}},
+                {room: "852", by: {id:"963",avatar: "img/girl.png", username: "girl", status: "offline"}},
+            ],
+
+            contacts: [
+                {type:undefined, id:"852",avatar: "img/punk.png",username:"punk",status:"online"},
+                {type:undefined, id:"741",avatar: "img/woman.png", username: "woman", status: "busy"},
+                {type:undefined, id:"963",avatar: "img/girl.png", username: "girl", status: "offline"},
+                {type:"order", id:"965",avatar: "img/hipster.png", username: "hipster", status: "offline"},
+                {type:"invite", id:"789",avatar: "img/rock.png", username: "rock", status: "admin"}
+            ],
+
+            messages:[{
+                id: "741963",
+                msg:"alguma mensagem do punk para afro",
+                room:"321",
+                by:{id:"987",avatar: "img/punk.png", username: "punk", status: "online"}
+            },
+                {
+                    id: "852963",
+                    msg:"alguma mensagem do afro para o seu amigo punk",
+                    room:"321",
+                    by:{id:"987",avatar: "img/afro.png", username: "afro", status: "online"}
+                },
+                {
+                    id: "741569",
+                    msg:"alguma mensagem do punk para afro",
+                    room:"321",
+                    by:{id:"987",avatar: "img/punk.png", username: "punk", status: "online"}
+                },
+                {
+                    id: "741963",
+                    msg:"alguma mensagem do afro para a sua amiga woman",
+                    room:"332",
+                    by:{id:"987",avatar: "img/afro.png", username: "afro", status: "online"}
+                },
+                {
+                    id: "741252",
+                    msg:"alguma mensagem da woman para a seu amigo afro",
+                    room:"332",
+                    by:{id:"987",avatar: "img/woman.png", username: "woman", status: "online"}
+                }]
+
+        }
     }
 
     componentDidMount() {
 
+    }
+
+    /// check container is has active
+    isContainer(container) {
+        return this.state.handler === container;
+    }
+
+    isHandlerClass(handler) {
+        return this.state.handler === handler ? "active" : ""
+    }
+
+    isConversationClass(room) {
+        return this.state.conversation.room === room ? "active" : "";
+    }
+
+    isConversation(room) {
+        return this.state.conversation.room === room
+    }
+
+    filterMessages(room) {
+        return this.state.messages.filter(msg => msg.room === room)
+    }
+
+
+    handlerConversation(conversation) {
+        this.setState({conversation})
+        this.handlerHandler("conversations")
+    }
+
+    handlerHandler = (handler) => {
+        this.setState({handler})
     }
 
     render() {
@@ -29,126 +111,116 @@ class App extends react.Component {
                         <div>
                             <div className={"unreadys"}>
                                 <ul>
-                                    <li><img width={"100%"} src={"img/fernanda.png"} alt={""} /></li>
+                                    <li><img width={"100%"} src={"img/fernanda.png"} alt={""}/></li>
                                 </ul>
                             </div>
                             <div className={"groups"}>
                                 <ul>
-                                    <li><img width={"100%"} src={"img/group.png"} alt={""} /></li>
+                                    <li><img width={"100%"} src={"img/group.png"} alt={""}/></li>
                                 </ul>
                             </div>
                         </div>
                         <div className={"handlers"}>
-                            <ul >
-                                <li className={"plus"}><i className="fas fa-plus" /></li>
+                            <ul>
+                                <li className={"plus"}><i className="fas fa-plus"/></li>
                             </ul>
                         </div>
                     </div>
-                    <div className={"navigation shadow"} >
+                    <div className={"navigation shadow"}>
                         <div className={"handlers"}>
                             <ul>
-                                <li><i className="fas fa-comment-dots"/></li>
-                                <li><i className="fas fa-user-alt" /></li>
-                                <li><i className="fas fa-cog" /></li>
+                                <li
+                                    className={this.isHandlerClass("conversations")}>
+                                <i className="fas fa-comment-dots"/></li>
+                                <li onClick={() => this.handlerHandler("contacts")}
+                                    className={this.isHandlerClass("contacts")}>
+                                    <i className="fas fa-user-alt"/></li>
+                                <li onClick={() => this.handlerHandler("cogs")}
+                                    className={this.isHandlerClass("cogs")}>
+                                    <i className="fas fa-cog"/></li>
                             </ul>
                         </div>
                         <div className={"profile"}>
-                            <img src={"img/jeferson.png"} alt={""} width={"100%"} />
+                            <img src={this.state.profile.avatar} alt={""} width={"100%"}/>
                             <div className={"profile-status online"}/>
                         </div>
                     </div>
                     <div className={"tab shadow"}>
                         <div className={"tab-container"}>
                             <ul>
-                                <li>
-                                    <div className={"li-avatar"}>
-                                        <img width={"35px"} src={"img/joelton.png"} alt={""} />
-                                        <div className={"li-status online"} />
-                                    </div>
-                                    <div className={"li-body"}>joelton</div>
-                                    <div className={"li-action"}><i className="fas fa-times" /></div>
-                                </li>
+                                {this.state.conversations.map(conversation => (
+                                    <li onClick={() => this.handlerConversation({room: conversation.room, to: conversation.by.id})}
+                                        className={this.isConversationClass(conversation.room)}>
+                                        <div className={"li-avatar"}>
+                                            <img width={"35px"} src={conversation.by.avatar} alt={""}/>
+                                            <div className={`li-status ${conversation.by.status}`}/>
+                                        </div>
+                                        <div className={"li-body"}>{conversation.by.username}</div>
+                                        <div className={"li-action"}><i className="fas fa-times"/></div>
+                                    </li>))}
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div className={"right shadow"}>
                     <div className={"container"}>
-                        <div className={"main"}>
-                            <div className={"header shadow"} />
-                            <div className={"room"}>
-                                <div className={"msg shadow"}>
-                                    <div className={"msg-avatar"}>
-                                        <img width={"35px"} src={"img/jeferson.png"} />
-                                    </div>
-                                    <div className={"msg-body"}>
-                                        The viewport-percentage lengths are relative to the size of the initial containing block. When the height or width of the initial containing block is changed
-                                    </div>
-                                </div>
-                                <div className={"msg shadow"}>
-                                    <div className={"msg-avatar"}>
-                                        <img width={"35px"} src={"img/bianca.png"} />
-                                    </div>
-                                    <div className={"msg-body"}>
-                                        The viewport-percentage lengths are relative to the size of the initial containing block. When the height or width of the initial containing block is  The viewport-percentage lengths are relative to the size of the initial containing block. When the height or width of the initial containing block is changed
-                                    </div>
-                                </div>
-                                <div className={"msg shadow"}>
-                                    <div className={"msg-avatar"}>
-                                        <img width={"35px"} src={"img/jeferson.png"} />
-                                    </div>
-                                    <div className={"msg-body"}>
-                                        When the height or width of the initial containing block is changed
-                                    </div>
-                                </div>
-                            </div>
+                        {this.isContainer("conversations") ? <div className={"main"}>
+                            <div className={"header shadow"}/>
+                            {this.state.conversations.map(conversation => (
+                                this.isConversation(conversation.room) ? <div className={"room"}>
+                                        {this.filterMessages(conversation.room).map(msg => (
+                                            <div className={"msg"}>
+                                               <div className={"msg-content  shadow"}>
+                                                   <div className={"msg-avatar"}>
+                                                       <img width={"35px"} src={msg.by.avatar}/>
+                                                   </div>
+                                                   <div className={"msg-body"}>
+                                                       {msg.msg}
+                                                   </div>
+                                               </div>
+                                            </div>
+                                        ))}
+                                </div>: ""
+                            ))}
                             <div className={"plaintext shadow"}>
                                 <div className={"plaintext-input"}>
                                     <input type={"text"}/>
                                 </div>
                                 <div className={"plaintext-action"}>
-                                    <i className="fas fa-grin-tongue-wink" />
+                                    <i className="fas fa-grin-tongue-wink"/>
                                 </div>
                             </div>
-                        </div>
-                        <div className={"main-container"}>
-                            {/*<div className={"header"}>*/}
-                            {/*    <div className={"invite-form"}>*/}
-                            {/*        <input type={"text"} />*/}
-                            {/*        <button><i className="fas fa-plus" /></button>*/}
-                            {/*    </div>*/}
-                            {/*</div>*/}
-                            {/*<div className={"tab-container contacts"}>*/}
-                            {/*    <ul>*/}
-                            {/*        <li>*/}
-                            {/*            <div className={"li-avatar"}>*/}
-                            {/*                <img width={"35px"} src={"img/joelton.png"} alt={""} />*/}
-                            {/*                <div className={"li-status online"} />*/}
-                            {/*            </div>*/}
-                            {/*            <div className={"li-body"}>joelton</div>*/}
-                            {/*            <div className={"li-action"}><i className="fas fa-bars" /></div>*/}
-                            {/*        </li>*/}
-                            {/*        <li className={"order"}>*/}
-                            {/*            <div className={"li-avatar"}>*/}
-                            {/*                <img width={"35px"} src={"img/bianca.png"} alt={""} />*/}
-                            {/*                <div className={"li-status online"} />*/}
-                            {/*            </div>*/}
-                            {/*            <div className={"li-body"}>bianca</div>*/}
-                            {/*            <div className={"li-action"}><i className="fas fa-bars"></i></div>*/}
-                            {/*        </li>*/}
-                            {/*        <li className={"invite"}>*/}
-                            {/*            <div className={"li-avatar"}>*/}
-                            {/*                <img width={"35px"} src={"img/beatriz.png"} alt={""} />*/}
-                            {/*                <div className={"li-status online"} />*/}
-                            {/*            </div>*/}
-                            {/*            <div className={"li-body"}>beatriz</div>*/}
-                            {/*            <div className={"li-action"}><i className="fas fa-bars"></i></div>*/}
-                            {/*        </li>*/}
-                            {/*    </ul>*/}
-                            {/*</div>*/}
-                        </div>
+                        </div> : ""}
+                        {this.isContainer("contacts") ? <div className={"main-container"}>
+                            <div className={"header"}>
+                                <div className={"invite-form"}>
+                                    <input type={"text"}/>
+                                    <button><i className="fas fa-plus"/></button>
+                                </div>
+                            </div>
+                            <div className={"tab-container contacts"}>
+                                <ul>
+                                    {this.state.contacts.map(contact => (
+                                        <li className={contact.type}>
+                                            <div className={"li-avatar"}>
+                                                <img width={"35px"} src={contact.avatar} alt={""}/>
+                                                {contact.type === undefined ? <div className={`li-status ${contact.status}`}/>: ""}
+                                            </div>
+                                            <div className={"li-body"}>{contact.username}</div>
+                                            <div className={"li-action"}><i className="fas fa-bars"/></div>
+                                        </li>
+                                    ))}
+
+                                </ul>
+                            </div>
+                        </div> : ""}
+                        {this.isContainer("cogs") ?
+                            <div className={"main-container"}>
+                                configurações
+                            </div> : ""}
+
                     </div>
-                    <div className={"wrappers shadow"} />
+                    <div className={"wrappers shadow"}/>
                 </div>
             </react.Fragment>
         )
